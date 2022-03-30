d5ce25ab65cbd382e25045b93fbf0cde999878dd
GregPapaspyropoulos <greg.papaspiropoulos@gmail.com>
2022-03-31 00:36:54 +0200

    minor fix and reformat

--- airport_db_greg.sql
+++ airport_db_greg.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE airport_db;
+
 USE airport_db;
 
 CREATE TABLE airlines (
@@ -15,18 +16,16 @@ CREATE TABLE airplane (
     Airline_ID int UNIQUE NOT NULL,
     Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     Updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    
     FOREIGN KEY (Airline_ID) REFERENCES airlines(Airline_ID)
 );
 
 CREATE TABLE pilots (
     PID int PRIMARY KEY,
     Pilot_Name VARCHAR(50) UNIQUE NOT NULL,
-    Salary FLOAT(9,2) UNIQUE NOT NULL,
+    Salary FLOAT(9, 2) UNIQUE NOT NULL,
     Airline_ID int UNIQUE NOT NULL,
     Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     Updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    
     FOREIGN KEY (Airline_ID) REFERENCES airlines(Airline_ID)
 );
 
@@ -38,19 +37,17 @@ CREATE TABLE flights (
     Departure VARCHAR(255) UNIQUE NOT NULL,
     Arrival VARCHAR(255) UNIQUE NOT NULL,
     Terminal_Number VARCHAR(50),
-	Date_of_departure DATE NOT NULL,
-	Has_Connection BIT(1),
+    Date_of_departure DATE NOT NULL,
+    Has_Connection BIT(1),
     Source_location VARCHAR(255) UNIQUE NOT NULL,
     Destination_location VARCHAR(255) UNIQUE NOT NULL,
     Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     Updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    
     FOREIGN KEY (Model_Number) REFERENCES airplane(Model_Number),
-	FOREIGN KEY (Airline_ID) REFERENCES airlines(Airline_ID),
-	FOREIGN KEY (PID) REFERENCES pilots(PID),
-	FOREIGN KEY (Source_location) REFERENCES airports(AP_Name),
-	FOREIGN KEY (Destination_location) REFERENCES airports(AP_Name)
-    
+    FOREIGN KEY (Airline_ID) REFERENCES airlines(Airline_ID),
+    FOREIGN KEY (PID) REFERENCES pilots(PID),
+    FOREIGN KEY (Source_location) REFERENCES airports(AP_Name),
+    FOREIGN KEY (Destination_location) REFERENCES airports(AP_Name)
 );
 
 CREATE TABLE passenger (
@@ -58,18 +55,17 @@ CREATE TABLE passenger (
     Age int UNIQUE NOT NULL,
     Sex VARCHAR(50) NOT NULL,
     Email VARCHAR(50) UNIQUE NOT NULL,
-	Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     Updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE ticket (
     Seat VARCHAR(50) NOT NULL UNIQUE,
     Flight_Code VARCHAR(255) NOT NULL,
-    Price FLOAT(9,2) NOT NULL,
+    Price FLOAT(9, 2) NOT NULL,
     Passport_Nr int NOT NULL,
-	Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     Updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    
     primary key (Seat, Flight_code),
     FOREIGN KEY (Passport_Nr) REFERENCES passenger(Passport_Nr)
 );
