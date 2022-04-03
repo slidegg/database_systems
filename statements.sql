08809d93e6a4661f6504d7511c29517e208cc681
danielschutt <danielschtt@gmail.com>
2022-04-03 09:07:13 +0200

    Daniel #7 #8

--- statements.sql
+++ statements.sql
@@ -13,3 +13,23 @@ HAVING
         FROM
             pilots
     );
+    
+    
+    
+    
+#Daniel - Display all airplanes each airline owns..
+select airL.airline_id, airline_name, planes.Model_Name, COUNT(planes.Model_Name) owned from airlines airL 
+left join airplane planes
+ on airL.airline_id = planes.airline_id
+ group by airL.airline_id;
+
+
+
+
+#Daniel - A store has had a namechange, and it must be reflected in the database.
+UPDATE airports set AP_Name =' Danish international airport' where city_Name = 'Copenhagen';
+
+select * from airports
+
+
+
