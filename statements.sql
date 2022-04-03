c0cb90dcd9ffbcbb166e9c9b41906002925262b1
s194591 <s194591@student.dtu.dk>
2022-04-03 09:26:32 +0200

    moved code

--- statements.sql
+++ statements.sql
@@ -14,7 +14,12 @@ HAVING
             pilots
     );
     
-    
+
+# Get sum of flights for all passengers, grouped by passport_nr - Oliver
+SELECT T.Passport_Nr, count(F.Flight_Code), P.Email FROM ticket T JOIN flights F JOIN (
+	SELECT Passport_Nr, Email from passenger) P 
+ON T.Flight_Code = F.Flight_Code AND T.Passport_Nr = P.Passport_Nr
+GROUP BY Passport_Nr;
     
     
 #Daniel - Display all airplanes each airline owns..
