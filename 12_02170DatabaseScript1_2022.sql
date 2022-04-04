9d787f2ba1807cdc1a5ed23ae075bb0112731348
s194591 <s194591@student.dtu.dk>
2022-04-04 14:58:16 +0200

    Final revisions

---
+++ 12_02170DatabaseScript1_2022.sql
@@ -0,0 +1,620 @@
+DROP DATABASE IF EXISTS airport_db;
+CREATE DATABASE airport_db;
+
+USE airport_db;
+
+CREATE TABLE airlines (
+    Airline_ID int PRIMARY KEY,
+    Airline_Name VARCHAR(50) UNIQUE NOT NULL,
+    IATA_Code VARCHAR(255) UNIQUE NOT NULL,
+    Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    Updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE stores (
+    Store_ID INT UNIQUE NOT NULL PRIMARY KEY,
+    Store_Name VARCHAR(250) NOT NULL,
+    Store_Type VARCHAR(250) NOT NULL,
+    Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    Updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE city (
+    City_Name VARCHAR(250) PRIMARY KEY,
+    Country VARCHAR(250) UNIQUE NOT NULL,
+    Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    Updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE airplane (
+    Model_number int PRIMARY KEY,
+    Model_Name VARCHAR(50) NOT NULL,
+    Airline_ID int,
+    Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    Updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (Airline_ID) REFERENCES airlines(Airline_ID) ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+CREATE TABLE pilots (
+    PID int PRIMARY KEY,
+    Pilot_Name VARCHAR(50) UNIQUE NOT NULL,
+    Salary FLOAT(9, 2) NOT NULL,
+    Airline_ID int,
+    Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    Updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (Airline_ID) REFERENCES airlines(Airline_ID) ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+CREATE TABLE airports (
+    AP_Name VARCHAR(250) UNIQUE NOT NULL PRIMARY KEY,
+    City_Name VARCHAR(250) UNIQUE NOT NULL,
+    Capacity VARCHAR(250) NOT NULL,
+    Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    Updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (City_Name) REFERENCES city(City_Name) ON UPDATE CASCADE
+);
+
+CREATE TABLE terminal (
+    Terminal_Number VARCHAR(50),
+    AP_Name VARCHAR(250),
+    Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    Updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (Terminal_Number, AP_Name),
+    FOREIGN KEY (AP_Name) REFERENCES airports(AP_Name) ON UPDATE CASCADE
+);
+
+CREATE TABLE timeslot (
+    Time_ID int PRIMARY KEY,
+    Time_Slot time(0) UNIQUE NOT NULL,
+    Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    Updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE flights (
+    Flight_code VARCHAR(255) PRIMARY KEY,
+    Model_Number int NOT NULL,
+    Airline_ID int NOT NULL,
+    Departure INT NOT NULL,
+    Arrival INT NOT NULL,
+    Terminal_Number VARCHAR(50),
+    Date_of_departure DATE NOT NULL,
+    Has_Connection BIT(1),
+    Source_location VARCHAR(255) NOT NULL,
+    Destination_location VARCHAR(255) NOT NULL,
+    Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    Updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (Model_Number) REFERENCES airplane(Model_Number) ON UPDATE CASCADE,
+    FOREIGN KEY (Terminal_Number) REFERENCES terminal(Terminal_Number) ON UPDATE CASCADE,
+    FOREIGN KEY (Airline_ID) REFERENCES airlines(Airline_ID) ON UPDATE CASCADE,
+    FOREIGN KEY (Source_location) REFERENCES airports(AP_Name) ON UPDATE CASCADE,
+    FOREIGN KEY (Destination_location) REFERENCES airports(AP_Name) ON UPDATE CASCADE,
+    FOREIGN KEY (Departure) REFERENCES timeslot(Time_ID),
+    FOREIGN KEY (Arrival) REFERENCES timeslot(Time_ID)
+);
+
+CREATE TABLE passenger (
+    Passport_Nr VARCHAR(50) PRIMARY KEY,
+    Age int UNIQUE NOT NULL,
+    Sex VARCHAR(50) NOT NULL,
+    Email VARCHAR(50) UNIQUE NOT NULL,
+    Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    Updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE ticket (
+    Seat VARCHAR(50) NOT NULL UNIQUE,
+    Flight_Code VARCHAR(255) NOT NULL,
+    Price FLOAT(9, 2) NOT NULL,
+    Passport_Nr VARCHAR(50) NOT NULL,
+    Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    Updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    primary key (Seat, Flight_code),
+    FOREIGN KEY (Passport_Nr) REFERENCES passenger(Passport_Nr) ON UPDATE CASCADE,
+    FOREIGN KEY (Flight_Code) REFERENCES flights(Flight_Code) ON DELETE CASCADE
+);
+
+CREATE TABLE flight_pilots (
+    PID int NOT NULL,
+    Flight_Code VARCHAR(255) NOT NULL,
+    Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    Updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    primary key (PID, Flight_Code),
+    FOREIGN KEY (PID) REFERENCES pilots(PID),
+    FOREIGN KEY (Flight_Code) REFERENCES flights(Flight_Code) ON DELETE CASCADE
+);
+
+CREATE TABLE airport_stores (
+	Store_ID int NOT NULL,
+    AP_Name VARCHAR(255) NOT NULL,
+    Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    Updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    primary key (Store_ID, AP_Name),
+    FOREIGN KEY (Store_ID) REFERENCES stores(Store_ID) ON DELETE CASCADE,
+    FOREIGN KEY (AP_Name) REFERENCES airports(AP_Name) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+#Population for airports and cities and terminals
+USE airport_db;
+
+SET SQL_SAFE_UPDATES = 0;
+
+DELETE FROM
+    city;
+
+insert into
+    city(City_Name, Country)
+Values
+    ('Budapest', 'Hungary'),
+    ('Copenhagen', 'Denmark'),
+    ('Lima', 'Peru'),
+    ('Luxembourg', 'Luxembourg'),
+    ('Oslo', 'Norway'),
+    ('Doha', 'Qatar'),
+    ('Tokyo', 'Japan'),
+    ('Paris', 'France');
+
+DELETE FROM
+    airports;
+
+insert into
+    airports(AP_Name, City_Name, Capacity)
+Values
+    ('Ferenc Liszt', 'Budapest', 500000),
+    ('Copenhagen airport', 'Copenhagen', 50000),
+    (
+        'Jorge Chavez international airport',
+        'Lima',
+        10000
+    ),
+    ('Luxembourg Findel airport', 'Luxembourg', 10000),
+    ('Oslo airport', 'Oslo', 10000),
+    ('Hamad International airport', 'Doha', 12500),
+    ('Haneda Airport', 'Tokyo', 30000),
+    ('Charles de gaulle airport', 'Paris', 9000);
+
+#Terminals
+DELETE FROM
+    terminal;
+
+insert into
+    terminal(Terminal_Number, AP_Name)
+Values
+    (1, 'Ferenc Liszt'),
+    (2, 'Ferenc Liszt'),
+    (3, 'Ferenc Liszt'),
+    (1, 'Copenhagen airport'),
+    (2, 'Copenhagen airport'),
+    (1, 'Jorge Chavez international airport'),
+    (2, 'Jorge Chavez international airport'),
+    (1, 'Luxembourg Findel airport'),
+    (2, 'Luxembourg Findel airport'),
+    (3, 'Luxembourg Findel airport'),
+    (1, 'Oslo airport'),
+    (1, 'Hamad International airport'),
+    (2, 'Hamad International airport'),
+    (1, 'Haneda Airport'),
+    (1, 'Charles de gaulle airport');
+
+DELETE FROM
+    timeslot;
+
+INSERT INTO
+    timeslot(Time_ID, Time_Slot)
+VALUES
+    (1, '00:00'),
+    (2, '00:15'),
+    (3, '00:30'),
+    (4, '00:45'),
+    (5, '01:00'),
+    (6, '01:15'),
+    (7, '01:30'),
+    (8, '01:45'),
+    (9, '02:00'),
+    (10, '02:15'),
+    (11, '02:30'),
+    (12, '02:45'),
+    (13, '03:00'),
+    (14, '03:15'),
+    (15, '03:30'),
+    (16, '03:45'),
+    (17, '04:00'),
+    (18, '04:15'),
+    (19, '04:30'),
+    (20, '04:45'),
+    (21, '05:00'),
+    (22, '05:15'),
+    (23, '05:30'),
+    (24, '05:45'),
+    (25, '06:00'),
+    (26, '06:15'),
+    (27, '06:30'),
+    (28, '06:45'),
+    (29, '07:00'),
+    (30, '07:15'),
+    (31, '07:30'),
+    (32, '07:45'),
+    (33, '08:00'),
+    (34, '08:15'),
+    (35, '08:30'),
+    (36, '08:45'),
+    (37, '09:00'),
+    (38, '09:15'),
+    (39, '09:30'),
+    (40, '09:45'),
+    (41, '10:00'),
+    (42, '10:15'),
+    (43, '10:30'),
+    (44, '10:45'),
+    (45, '11:00'),
+    (46, '11:15'),
+    (47, '11:30'),
+    (48, '11:45'),
+    (49, '12:00'),
+    (50, '12:15'),
+    (51, '12:30'),
+    (52, '12:45'),
+    (53, '13:00'),
+    (54, '13:15'),
+    (55, '13:30'),
+    (56, '13:45'),
+    (57, '14:00'),
+    (58, '14:15'),
+    (59, '14:30'),
+    (60, '14:45'),
+    (61, '15:00'),
+    (62, '15:15'),
+    (63, '15:30'),
+    (64, '15:45'),
+    (65, '16:00'),
+    (66, '16:15'),
+    (67, '16:30'),
+    (68, '16:45'),
+    (69, '17:00'),
+    (70, '17:15'),
+    (71, '17:30'),
+    (72, '17:45'),
+    (73, '18:00'),
+    (74, '18:15'),
+    (75, '18:30'),
+    (76, '18:45'),
+    (77, '19:00'),
+    (78, '19:15'),
+    (79, '19:30'),
+    (80, '19:45'),
+    (81, '20:00'),
+    (82, '20:15'),
+    (83, '20:30'),
+    (84, '20:45'),
+    (85, '21:00'),
+    (86, '21:15'),
+    (87, '21:30'),
+    (88, '21:45'),
+    (89, '22:00'),
+    (90, '22:15'),
+    (91, '22:30'),
+    (92, '22:45'),
+    (93, '23:00'),
+    (94, '23:15'),
+    (95, '23:30'),
+    (96, '23:45');
+
+DELETE FROM
+    passenger;
+
+insert into
+    passenger(Passport_Nr, Age, Sex, Email)
+Values
+    (
+        'GR23232311',
+        '25',
+        'MALE',
+        'passenger_gr@dtu.com'
+    ),
+    (
+        'BU35265464',
+        '26',
+        'MALE',
+        'passenger_bu@dtu.com'
+    ),
+    (
+        'DA54416575',
+        '32',
+        'FEMALE',
+        'passenger_da@dtu.com'
+    ),
+    (
+        'DE54546422',
+        '40',
+        'FEMALE',
+        'passenger_de@dtu.com'
+    ),
+    (
+        'US15444644',
+        '50',
+        'FEMALE',
+        'passenger_us@dtu.com'
+    ),
+    (
+        'IT15413214',
+        '18',
+        'MALE',
+        'passenger_it@dtu.com'
+    ),
+    (
+        'UK54613146',
+        '21',
+        'FEMALE',
+        'passenger_uk@dtu.com'
+    ),
+    (
+        'FR54416198',
+        '70',
+        'FEMALE',
+        'passenger_fr@dtu.com'
+    );
+
+DELETE FROM
+    airlines;
+
+INSERT INTO
+    airlines(Airline_ID, Airline_Name, IATA_Code)
+VALUES
+    (0, 'SAS', 'SK'),
+    (1, 'Ryanair', 'FR'),
+    (2, 'Lufthansa Group', 'LH'),
+    (3, 'Delta Air Lines', 'DL'),
+    (4, 'China Southern Airlines', 'CZ');
+
+DELETE FROM
+    pilots;
+
+INSERT INTO
+    pilots(PID, Pilot_Name, Salary, Airline_ID)
+VALUES
+    (0, "Adams Armstrong", 45000, 0),
+    (1, "James Hansen", 40500, 0),
+    (2, "Robert Neil", 43200, 1),
+    (3, "John Depp", 45100, 1),
+    (4, "Michael Jackson", 38800, 2),
+    (5, "William Macbeth", 39600, 2),
+    (6, "David Anderson", 36300, 3),
+    (7, "Kenneth Pløger", 55000, 3),
+    (8, "Nadia Antonio", 46200, 3),
+    (9, "Rabija Gandhi", 41500, 4),
+    (10, "Sun Tzu", 39500, 4),
+    (11, "Wi Tu Lo", 38000, 4);
+
+DELETE FROM
+    airplane;
+
+INSERT INTO
+    airplane(Model_Number, Model_Name, Airline_ID)
+VALUES
+    (10120, "Sukhoi Superjet 100", 0),
+    (10320, "Boeing 737-800", 1),
+    (10121, "Sukhoi Superjet 100", 2),
+    (11230, "Airbus A330-300", 3),
+    (11231, "Airbus A330-300", 3),
+    (12310, "Embraer 170/175", 4),
+    (12311, "Embraer 170/175 ", 4);
+
+DELETE FROM
+    flights;
+INSERT INTO
+    flights(
+        Flight_Code,
+        Model_Number,
+        Airline_ID,
+        Departure,
+        Arrival,
+        Terminal_Number,
+        Date_of_departure,
+        Has_Connection,
+        Source_location,
+        Destination_location
+    )
+VALUES
+    (
+        100,
+        10120,
+        0,
+        74,
+        78,
+        1,
+        curdate(),
+        0,
+        'Copenhagen airport',
+        'Ferenc Liszt'
+    ),
+    (
+        101,
+        10320,
+        1,
+        14,
+        22,
+        1,
+        curdate(),
+        0,
+        'Ferenc Liszt',
+        'Oslo airport'
+    ),
+    (
+        102,
+        10121,
+        2,
+        12,
+        18,
+        2,
+        curdate(),
+        0,
+        'Copenhagen airport',
+        'Jorge Chavez international airport'
+    ),
+    (
+        103,
+        11230,
+        3,
+        31,
+        40,
+        1,
+        curdate(),
+        1,
+        'Haneda Airport',
+        'Copenhagen airport'
+    ),
+    (
+        104,
+        11231,
+        3,
+        31,
+        35,
+        2,
+        curdate(),
+        0,
+        'Jorge Chavez international airport',
+        'Copenhagen airport'
+    ),
+    (
+        105,
+        12310,
+        4,
+        12,
+        22,
+        3,
+        curdate(),
+        1,
+        'Luxembourg Findel airport',
+        'Ferenc Liszt'
+    ),
+    (
+        106,
+        10121,
+        2,
+        14,
+        24,
+        1,
+        curdate(),
+        0,
+        'Oslo airport',
+        'Haneda Airport'
+    ),
+    (
+        107,
+        12311,
+        4,
+        2,
+        22,
+        1,
+        curdate(),
+        0,
+        'Charles de gaulle airport',
+        'Haneda Airport'
+    ),
+    (
+        108,
+        12311,
+        4,
+        83,
+        90,
+        2,
+        curdate(),
+        0,
+        'Ferenc Liszt',
+        'Oslo airport'
+    ),
+    (
+        109,
+        10320,
+        1,
+        74,
+        84,
+        1,
+        curdate(),
+        1,
+        'Charles de gaulle airport',
+        'Ferenc Liszt'
+    );
+
+DELETE FROM ticket;
+insert into ticket(Seat, Flight_Code, Price, Passport_Nr)
+Values ('1a', '100', 300, 'GR23232311'),
+	   ('12b', '101', 275, 'GR23232311'),
+	   ('2c', '101', 400, 'BU35265464'),
+	   ('10a', '102', 350, 'DA54416575'),
+	   ('10b', '103', 120, 'DE54546422'),
+	   ('15c', '104', 750, 'US15444644'),
+	   ('15b', '105', 1250, 'IT15413214'),
+	   ('20a', '106', 900, 'UK54613146'),
+	   ('20b', '108', 850, 'FR54416198');
+
+DELETE FROM
+    flight_pilots;
+INSERT INTO
+    flight_pilots(PID, Flight_Code)
+VALUES
+    (0, 100),
+    (1, 100),
+    (2, 101),
+    (3, 101),
+    (4, 101),
+    (3, 102),
+    (5, 103),
+    (6, 103),
+    (5, 104),
+    (7, 104),
+    (8, 105),
+    (9, 105),
+    (1, 106),
+    (2, 106),
+    (5, 107),
+    (10, 108),
+    (11, 109);
+
+DELETE FROM
+    stores;
+
+INSERT INTO
+    stores(Store_ID, Store_Name, Store_Type)
+VALUES
+    (0, "StarBucks", "Food & Drinks"),
+    (1, "WHSmith", "Books & Magazines"),
+    (2, "Airport Pharmacy", "Medicine"),
+    (3, "ECCO", "Fashion"),
+    (4, "Global Exchange", "Currency"),
+    (5, "LEGO", "Toys"),
+    (6, "Gorm's", "Food & Drinks");
+
+DELETE FROM
+    airport_stores;
+
+INSERT INTO
+    airport_stores(Store_ID, AP_Name)
+VALUES
+    (0, "Ferenc Liszt"),
+    (0, "Jorge Chavez international airport"),
+    (0, "Oslo airport"),
+    (1, "Ferenc Liszt"),
+    (1, "Copenhagen airport"),
+    (1, "Hamad International airport"),
+    (2, "Copenhagen airport"),
+    (2, "Jorge Chavez international airport"),
+    (2, "Haneda Airport"),
+    (3, "Copenhagen airport"),
+    (3, "Oslo airport"),
+    (3, "Charles de gaulle airport"),
+    (4, "Ferenc Liszt"),
+    (4, "Haneda Airport"),
+    (4, "Charles de gaulle airport"),
+    (5, "Jorge Chavez international airport"),
+    (5, "Haneda Airport"),
+    (5, "Charles de gaulle airport"),
+    (6, "Ferenc Liszt"),
+    (6, "Luxembourg Findel airport"),
+    (6, "Hamad International airport"),
+    (0, "Luxembourg Findel airport"),
+    (1, "Oslo airport"),
+    (2, "Hamad International airport"),
+    (3, "Hamad International airport"),
+    (4, "Hamad International airport"),
+    (6, "Haneda Airport"),
+    (5, "Copenhagen airport");
---
+++ 12_02170DatabaseScript2_2022.sql
@@ -0,0 +1,123 @@
+# Greg - Select show all pilots name and salaries with higher salary than the avg salary
+SELECT
+    Pilot_Name,
+    Salary
+FROM
+    pilots
+GROUP BY
+    Salary
+HAVING
+    Salary > (
+        SELECT
+            AVG(Salary)
+        FROM
+            pilots
+    );
+    
+# Sabina Select & Update
+SELECT Passport_Nr, Email, Flight_Code, Seat FROM passenger JOIN ticket USING (Passport_Nr) ORDER BY Passport_Nr ASC;
+UPDATE pilots SET Salary = Salary * 1.10 WHERE Airline_ID = 3;
+
+# Get sum of flights for all passengers, grouped by passport_nr - Oliver
+SELECT T.Passport_Nr, count(F.Flight_Code) as NumFlights, P.Email FROM ticket T JOIN flights F JOIN (
+	SELECT Passport_Nr, Email from passenger) P 
+ON T.Flight_Code = F.Flight_Code AND T.Passport_Nr = P.Passport_Nr
+GROUP BY Passport_Nr;
+
+# TRANSACTION PROCEDURE - Oliver
+DROP PROCEDURE IF EXISTS ChangeFlightDeparture;
+DELIMITER \\
+CREATE PROCEDURE ChangeFlightDeparture(
+	IN vFlight_Code VARCHAR(255), vNewTimeSlotID int, vNewTerminalNum VARCHAR(50), OUT vStatus VARCHAR(255))
+BEGIN
+	DECLARE AnyFlightsAtNewTime INT DEFAULT 0;
+    DECLARE AirportName VARCHAR(255) DEFAULT "";
+    DECLARE AirportMaxTerminals INT DEFAULT 0;
+    DECLARE DOD DATE DEFAULT curdate();
+    START TRANSACTION;
+    SET AirportName = (SELECT Source_location FROM flights WHERE vFlight_Code = Flight_Code);
+    SET AirportMaxTerminals = (SELECT count(*) FROM terminal WHERE AP_Name = AirportName);
+    IF AirportMaxTerminals < vNewTerminalNum
+		THEN SET vStatus = 'Terminal Number does not exists for the current airport, rollback!'; ROLLBACK;
+        ELSE UPDATE flights SET Departure = vNewTimeSlotID, Terminal_Number = vNewTerminalNum WHERE Flight_Code = vFlight_Code;
+			 SET DOD = (SELECT Date_of_departure FROM flights WHERE Flight_Code = vFlight_Code);
+			 SET AnyFlightsAtNewTime = (SELECT count(*) FROM flights F WHERE vNewTimeSlotID = F.Departure AND DOD = F.Date_of_departure AND F.Terminal_Number = vNewTerminalNum AND F.Source_location = AirportName);
+			 IF AnyFlightsAtNewTime > 1
+				THEN SET vStatus = 'A flight departure update would conflict with an existing one, rollback!'; ROLLBACK;
+				ELSE SET vStatus = 'Flight has been updated, committed!'; COMMIT;
+			 END IF; 
+    END IF;
+END \\
+DELIMITER ;
+
+CALL ChangeFlightDeparture(100, 74, 1, @tStatus);
+SELECT @tStatus;
+SELECT * FROM flights WHERE Flight_Code = 100;
+
+CALL ChangeFlightDeparture(100, 12, 2, @tStatus);
+SELECT @tStatus;
+SELECT * FROM flights WHERE Flight_Code = 100;
+    
+#Daniel - Display all airplanes each airline owns..
+select airL.airline_id, airline_name, planes.Model_Name, COUNT(planes.Model_Name) owned from airlines airL 
+left join airplane planes
+ on airL.airline_id = planes.airline_id
+ group by airL.airline_id;
+
+
+
+
+#Daniel - A store has had a namechange, and it must be reflected in the database.
+UPDATE airports set AP_Name =' Danish international airport' where city_Name = 'Copenhagen';
+
+select * from airports;
+
+# Greg - A trigger for checking salary before inserting to table of pilots
+DROP TRIGGER IF EXISTS upd_salary_check;
+
+delimiter //
+
+CREATE TRIGGER upd_salary_check BEFORE INSERT ON pilots FOR EACH ROW IF NEW.Salary < 0 
+THEN SET NEW.Salary = 0; 
+END IF; //  
+
+# Greg Test
+INSERT INTO
+    pilots(PID, Pilot_Name, Salary, Airline_ID)
+VALUES
+    (7000, "Test Trigger", -45000, 4);
+    
+SELECT Salary FROM pilots WHERE PID = 7000;
+
+
+# Daniel Function
+DELIMITER //
+CREATE FUNCTION flightDuration(Departure int,Arrival int) RETURNS time
+BEGIN
+	DECLARE difference time;
+	DECLARE arrivaltime datetime;
+	DECLARE departuretime datetime;
+	SELECT CAST(time_slot AS datetime) from timeslot where arrival = time_id INTO
+	arrivaltime;
+	SELECT CAST(time_slot AS datetime) from timeslot where departure= time_id INTO
+	departuretime;
+	SELECT SEC_TO_TIME(TIMESTAMPDIFF(SECOND, arrivaltime,departuretime)) into difference;
+	RETURN difference ;
+END//
+DELIMITER ;
+
+# Daniel Test Function
+select flight_code, flightDuration(arrival,departure) duration from flights;
+
+# Patrick Event
+DROP EVENT IF EXISTS ‘deleteDepartedFlights‘;
+DELIMITER $$
+CREATE EVENT `deleteDepartedFlights`
+  ON SCHEDULE EVERY 1 DAY STARTS '2022-01-01 00:00:00'
+  ON COMPLETION PRESERVE
+DO BEGIN
+   DELETE FROM Flights 
+   WHERE datediff(curdate(), Date_of_departure)>=1;
+   
+END$$
+DELIMITER ;
